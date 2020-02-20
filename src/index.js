b82a640332b8dabdb035d4306338d786612b3ca9
ElfimovArtem <elfimov021@yandex.ru>
2020-02-20 11:41:46 +0300

    feat: decided task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.min = function min (array=[]) {
+    if (array.length > 0) {
+        return Math.min(...array);
+    } else {
+        return 0;
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max (array=[]) {
+    if (array.length > 0) {
+        return Math.max(...array);
+    } else {
+        return 0;
+    }
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg (array=[]) {
+    if (array.length > 0) {
+        const sum = array.reduce((acc, cV) => acc + cV, 0);
+        const n = array.length;
+        return sum / n;
+    } else {
+        return 0;
+    }
+
+};
